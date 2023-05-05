c4017c9983b7184dd5aca2ea66b7a6bd59d2ea24
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-05-05 21:44:48 +0300

    programmatic imageView

--- CardWorkout-Programmatic/CardWorkout-Programmatic/CardSelectionVC.swift
+++ CardWorkout-Programmatic/CardWorkout-Programmatic/CardSelectionVC.swift
@@ -11,15 +11,23 @@ class CardSelectionVC: UIViewController {
         super.viewDidLoad()
         //Background
         view.backgroundColor = .systemBackground
+        configureUI()
     }
 
     func configureUI() {
-        
+        configureCardImageView()
     }
     
     func configureCardImageView() {
         view.addSubview(cardImageView)
         cardImageView.translatesAutoresizingMaskIntoConstraints = false
-        cardImageView.image = UIImage(named: "")
+        cardImageView.image = UIImage(named: "AS")
+        
+        NSLayoutConstraint.activate([
+            cardImageView.widthAnchor.constraint(equalToConstant: 250),
+            cardImageView.heightAnchor.constraint(equalToConstant: 350),
+            cardImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            cardImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -85)
+        ])
     }
 }
