4e7f1aa0f187bef6e516f5c7f038137a25783f85
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-05-05 15:41:03 +0300

    UIButton

--- CardWorkout-Programmatic/CardWorkout-Programmatic/CardSelectionVC.swift
+++ CardWorkout-Programmatic/CardWorkout-Programmatic/CardSelectionVC.swift
@@ -1,10 +1,14 @@
 import UIKit
 
 class CardSelectionVC: UIViewController {
+    
+    let carImageView  = UIImageView()
+    let stopButton    = CWButton(backgroundColor: .systemRed , title: "Stop!")
+    let resetButton   = CWButton(backgroundColor: .systemGreen, title: "Reset")
+    let rulesButton   = CWButton(backgroundColor: .systemBlue, title: "Rules")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .systemBackground
         
     }
