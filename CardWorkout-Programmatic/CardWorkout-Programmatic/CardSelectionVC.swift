f3da7ca3e403d811d9308b024b329678dd1ec162
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-05-05 21:35:13 +0300

    UIImage

--- CardWorkout-Programmatic/CardWorkout-Programmatic/CardSelectionVC.swift
+++ CardWorkout-Programmatic/CardWorkout-Programmatic/CardSelectionVC.swift
@@ -1,16 +1,25 @@
 import UIKit
 
 class CardSelectionVC: UIViewController {
-    
-    let carImageView  = UIImageView()
+    //3 Button and ImageView
+    let cardImageView  = UIImageView()
     let stopButton    = CWButton(backgroundColor: .systemRed , title: "Stop!")
     let resetButton   = CWButton(backgroundColor: .systemGreen, title: "Reset")
     let rulesButton   = CWButton(backgroundColor: .systemBlue, title: "Rules")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Background
         view.backgroundColor = .systemBackground
-        
     }
 
+    func configureUI() {
+        
+    }
+    
+    func configureCardImageView() {
+        view.addSubview(cardImageView)
+        cardImageView.translatesAutoresizingMaskIntoConstraints = false
+        cardImageView.image = UIImage(named: "")
+    }
 }
