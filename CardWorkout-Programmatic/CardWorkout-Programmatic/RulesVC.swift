8d1391fa8f96bbf276f5d904510b90d51c750f01
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-05-06 10:33:25 +0300

    Rules

---
+++ CardWorkout-Programmatic/CardWorkout-Programmatic/RulesVC.swift
@@ -0,0 +1,19 @@
+//
+//  RulesVC.swift
+//  CardWorkout-Programmatic
+//
+//  Created by Serdar Altındaş on 6.05.2023.
+//
+
+import UIKit
+
+class RulesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+        
+    }
+
+}
